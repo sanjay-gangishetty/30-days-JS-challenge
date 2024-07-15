38a8abc9b8ec84dad15d1bce302835d5b9f508c4
Sanjay <gangishettysanjaykumar@gmail.com>
2024-07-15 21:48:39 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,20 @@
+// Activity 1 - Variable Declaration
+var number = 10;
+console.log(number);
+let string = "Hello World";
+console.log(string);
+const boolean = true;
+console.log(boolean);
+
+const num = 10; const str = "Hello World"; const bool = true; const obj = {name: "Sanjay", age: 25}; 
+const arr = [1,2,3,4,5,6];
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+let a = 10;
+console.log("Before reassigning: ", a);
+a = 20;
+console.log("After reassigning: ", a);
