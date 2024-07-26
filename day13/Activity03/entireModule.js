c59a53150b8299fd22b008184ae3c25e120ef4aa
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-07-26 06:45:20 +0530

    Create entireModule.js

---
+++ day13/Activity03/entireModule.js
@@ -0,0 +1,8 @@
+// Task 5: Create a module that exports multiple constants and functions. Import the entire module as an object in another script and use its properties.
+export const name = "Sanjay";
+export const age = 23;
+
+export function greet(){
+  return "Hello JS";
+}
+
