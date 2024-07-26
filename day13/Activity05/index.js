5d942e4538930f344c6b9967bd03d29f527a2b63
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-07-26 06:46:45 +0530

    Create index.js

---
+++ day13/Activity05/index.js
@@ -0,0 +1,2 @@
+// Activity 5: Module Bundling
+// Task 8: Use a module bundler like Webpack or Parcel to bundle multiple JavaScript files into a single file. Write a script to demonstrate the bundling process.
