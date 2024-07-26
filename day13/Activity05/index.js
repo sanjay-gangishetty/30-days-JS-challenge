27960658dd4f73e1c266dfc3c24df0f6aa618d15
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-07-26 07:23:04 +0530

    Update and rename index.js to webpack.config.js

--- day13/Activity05/index.js
+++
@@ -1,2 +0,0 @@
-// Activity 5: Module Bundling
-// Task 8: Use a module bundler like Webpack or Parcel to bundle multiple JavaScript files into a single file. Write a script to demonstrate the bundling process.
