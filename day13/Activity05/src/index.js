3a7dfd68af08e5f07d02e67d309686a3213a9f70
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-07-26 07:24:00 +0530

    Update index.js

--- day13/Activity05/src/index.js
+++ day13/Activity05/src/index.js
@@ -1,3 +1,5 @@
+// Activity 5: Module Bundling
+// Task 8: Use a module bundler like Webpack or Parcel to bundle multiple JavaScript files into a single file. Write a script to demonstrate the bundling process.
 import { greet } from './module1';
 import { farewell } from './module2';
 
