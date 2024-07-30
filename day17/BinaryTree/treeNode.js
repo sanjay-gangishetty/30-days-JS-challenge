4ccd84068ca5170d72df28c9097601e6e2651314
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-07-30 06:21:14 +0530

    Create treeNode.js

---
+++ day17/BinaryTree/treeNode.js
@@ -0,0 +1,8 @@
+// Implement a tree node class to represent a node in a binary tree with the properties value, left, right.
+class treeNode{
+  constructor(value){
+    this.value = value,
+    this.left = null,
+    this.right = null
+  }
+}
